c4cb79cc1f13f0b6b9c15ad5afa7dc76d22575e9
Julian Verschuren <julianverschuren@hotmail.com>
2016-03-14 19:52:54 +0100

    weer wat verder
    weer wat verder

--- modaalVenster.js
+++ modaalVenster.js
@@ -1,10 +1,13 @@
 var objectModalWindow = (function () {
 
-	$modalWindow = $('<div class="modal-window"/>'); //maakt gelijk een class aan genaamd modal-window, opgeslagen in een variabele modalWindow, zo kan ik de css aanpassen 
 
-	$contentsModal = $('<div class="contents-Modal"/>'); //maakt gelijk een class aan genaamd contents-window, voor dit geld hetzelfde als de gene hierboven.
+	var $window = $(window); // variabele window word vaak zo genoemd in jquery. Is een jquery object. het browser venster. 
 
-	$buttonClose = $('<span class="button-Close">&%10005;</span>'); //maakt gelijk een class aan genaamd button-close. ook hierbij is het zo dat ik de css kan aanpassen. er zit ook een preset button style inverwerkt. 
+	var $modalWindow = $('<div class="modal-window"/>'); //maakt gelijk een class aan genaamd modal-window, opgeslagen in een variabele modalWindow, zo kan ik de css aanpassen 
+
+	 var $contentsModal = $('<div class="contents-Modal"/>'); //maakt gelijk een class aan genaamd contents-window, voor dit geld hetzelfde als de gene hierboven.
+
+	var $buttonClose = $('<span class="button-Close">&#10005;</span>'); //maakt gelijk een class aan genaamd button-close. ook hierbij is het zo dat ik de css kan aanpassen. er zit ook een preset button style inverwerkt. 
 
 
 	$modalWindow.append($contentsModal); //gooit de contents in het venster.
@@ -18,22 +21,30 @@ var objectModalWindow = (function () {
 		center: function () {
 			//opdrachten centreren
 
+			var links = Math.max($window.width()-$contentsModal.outerWidth(), 0)/2; 
+			var boven = Math.max($window.height()-$contentsModal.outerHeight(), 0)/2; 
+			$contentsModal.css({left: links, top: boven});
 
 		},
 
 		open: function (settings) { //functie om er voor te zorgen dat het venster opent. , wanneer het opent is bepaald in modaalstart. 
-
-			$contentsModal.append(settings.inhoud);
-			$contentsModal.css('width', settings.breedte+'px'); //geef aan dat je de breedte wilt gebruiken die in modaalstart is aangegeven
+				//opdrachten openen
+			$contentsModal.append(settings.inhoud, $buttonClose);
+			$contentsModal.css({width: settings.breedte+'px' ||'auto'
+							  , height: settings.hoogte+'px' || 'auto'}); //geef aan dat je de breedte wilt gebruiken die in modaalstart is aangegeven
 			$modalWindow.appendTo('body'); //zet het venster op de body
+			objectModalWindow.center();
+			$buttonClose.on('click', objectModalWindow.sluiten); //click event nodig voor het sluiten van de venster op de knop.
+			$window.on('resize', objectModalWindow.center); //resised en centered als het scherm verkleint wordt.
 		},
 
 		sluiten: function () {
 
 			//opdrachten sluiten
-
-
-
+			console.log('Closing window'); //logt het feit dat het venster gesloten is
+			$modalWindow.detach(); //haalt het complet venster weg
+			$contentsModal.empty(); //leegt de inhoud van het venster
+			$window.off('resize', objectModalWindow.center);
 		}
 
 
