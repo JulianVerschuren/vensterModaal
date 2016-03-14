70e54d579e0d8cb05d1219debe0b7d9d6653f219
Julian Verschuren <julianverschuren@hotmail.com>
2016-03-14 17:54:04 +0100

    Venster verfraait, functionaliteit toegevoegd
    Venster wat verbeterd kwa uiterlijk. Functionaliteit van eventlisteners toegevoegd. veel info  met comments toegevoegd

--- modaalStart.js
+++ modaalStart.js
@@ -11,5 +11,36 @@ var cobainContents = $('#cobain').detach(); //hierbij ook weer hetzelfde verhaal
 
 //plaatsen van het modale venster
 
-objectModalWindow.open(hendrixContents);
-})
+//objectModalWindow.open(hendrixContents);
+
+
+//events met thumbnail, dus het klikken op het plaatje komt hieronder:
+
+
+
+$('#jimiThumb').on('click', function(){
+
+objectModalWindow.open({inhoud: hendrixContents, breedte: 600});
+
+
+}); //dit zorgt ervoor dat op het moment dat ik op de thumbnail/foto klik van jimi hendrix,  de content opent die ik in de eerste instantie gedetached had.
+
+$('#morriThumb').on('click', function(){
+
+objectModalWindow.open({inhoud: morissonContents, breedte: 600});
+
+
+}); //hierin heb ik ook de breedte aangegeven. de breedte heb ik op 600 gelaten, vind het mooier om het even groot te houden.
+
+$('#kurtThumb').on('click', function(){
+
+objectModalWindow.open({inhoud: cobainContents, breedte: 600});
+
+//objectmodalwindow is een functie die ik gemaakt heb in modaalvenster ik heb onderandere div classes toegevoegd aan een aantal nieuw gemaakte variabelen zoals:
+//modalwindow, buttonclose, en contentsmodel. de functie heb ik nodig voor het openen, sluiten en centeren van het venster, met verschillende inhouden. hier roep ik objectmodalwindow dus aan en zeg dat ie de method open moet gebruiken met de inhoud van de verschillende contents met breedte. 
+});
+
+
+});
+
+
--- modaalVenster.js
+++ modaalVenster.js
@@ -1,13 +1,13 @@
 var objectModalWindow = (function () {
 
-	$modalWindow = $('<div class="modal-window"/>');
+	$modalWindow = $('<div class="modal-window"/>'); //maakt gelijk een class aan genaamd modal-window, opgeslagen in een variabele modalWindow, zo kan ik de css aanpassen 
 
-	$contentsModal = $('<div class="contents-Modal"/>');
+	$contentsModal = $('<div class="contents-Modal"/>'); //maakt gelijk een class aan genaamd contents-window, voor dit geld hetzelfde als de gene hierboven.
 
-	$buttonClose = $('<span class="button-Close">&%10005;</span>');
+	$buttonClose = $('<span class="button-Close">&%10005;</span>'); //maakt gelijk een class aan genaamd button-close. ook hierbij is het zo dat ik de css kan aanpassen. er zit ook een preset button style inverwerkt. 
 
 
-	$modalWindow.append($contentsModal);
+	$modalWindow.append($contentsModal); //gooit de contents in het venster.
 
 
 	//hieronder ga ik 3 methods definieren.
@@ -21,11 +21,11 @@ var objectModalWindow = (function () {
 
 		},
 
-		open: function (settings) {
+		open: function (settings) { //functie om er voor te zorgen dat het venster opent. , wanneer het opent is bepaald in modaalstart. 
 
-			console.log('het venster openen met: ' + settings);
-			$modalWindow.appendTo('body');
-
+			$contentsModal.append(settings.inhoud);
+			$contentsModal.css('width', settings.breedte+'px'); //geef aan dat je de breedte wilt gebruiken die in modaalstart is aangegeven
+			$modalWindow.appendTo('body'); //zet het venster op de body
 		},
 
 		sluiten: function () {
